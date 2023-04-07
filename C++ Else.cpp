253c711392880f268771e93e1f56cb0b033b190c
sai_tejas <saitejassharma@gmail.com>
2023-04-07 23:30:38 +0530

    Create C++ Else.cpp

---
+++ C++ Else.cpp
@@ -0,0 +1,14 @@
+> Ex : 1 - The else Statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int time = 20;
+  if (time < 18) {
+    cout << "Good day.";
+  } else {
+    cout << "Good evening.";
+  }
+  return 0;
+}                                      # Outputs "Good evening."
