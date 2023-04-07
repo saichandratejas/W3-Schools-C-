7e79bf69a5fcfc1483a6f71a6203a5ff4c65de85
sai_tejas <saitejassharma@gmail.com>
2023-04-07 23:17:01 +0530

    Create C++ Math.cpp

---
+++ C++ Math.cpp
@@ -0,0 +1,38 @@
+> Ex : 1
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << max(5, 10);
+  return 0;
+}                            # 10
+  
+  
+> Ex : 2
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << min(5, 10);
+  return 0;
+}                             # 5
+  
+
+> Ex : 3 - C++ <cmath> Header
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << sqrt(64) << "\n";
+  cout << round(2.6) << "\n";
+  cout << log(2) << "\n";
+  return 0;
+}                              # 8
+                               # 3
+                               # 0.693147
+
+
