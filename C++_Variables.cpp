ffb7152032fad962a69d41f5f4ef7b154264cf45
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:11:40 +0530

    Update C++_Variables.cpp

--- C++_Variables.cpp
+++ C++_Variables.cpp
@@ -1,8 +1,60 @@
+> Ex: 1 - Declaring (Creating) Variables
+
 #include <iostream>
 using namespace std;
 
 int main() {
   int myNum = 15;
   cout << myNum;
-  return 0;
+  return 0;             # Result is : 15
 }
+
+> Ex: 2 - Declaring (Creating) Variables
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum;
+  myNum = 15;
+  cout << myNum;
+  return 0;             # Result is : 15
+}
+
+> Ex: 3 - Declaring (Creating) Variables
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 15;   // Now myNum is 15
+  myNum = 10;       // Now myNum is 10
+  cout << myNum;
+  return 0;
+}                      #  Result is : 10
+
+> Ex: 4 - Display Variables
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 35;
+  cout << "I am " << myAge << " years old.";
+  return 0;
+}                      # Result is : I am 35 years old.
+  
+
+> Ex: 5 - Add Variables Together
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 6;
+  int sum = x + y;
+  cout << sum;
+  return 0;
+}                     # Result is : 11
+
