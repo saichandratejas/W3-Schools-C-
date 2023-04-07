76182c53e9b332e2e3e3161f2c64ff086db796a5
sai_tejas <saitejassharma@gmail.com>
2023-04-08 01:17:44 +0530

    Create C++ References.cpp

---
+++ C++ References.cpp
@@ -0,0 +1,11 @@
+Creating References:
+- A reference variable is a "reference" to an existing variable, and it is created with the & operator
+  
+>> Ex:
+
+string food = "Pizza";  // food variable
+string &meal = food;    // reference to food 
+
+// output :
+Pizza
+Pizza
