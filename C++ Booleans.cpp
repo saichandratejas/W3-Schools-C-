088ae54f02cbad859427ced753b3902143d6397a
sai_tejas <saitejassharma@gmail.com>
2023-04-07 23:17:59 +0530

    Create C++ Booleans.cpp

---
+++ C++ Booleans.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  bool isCodingFun = true;
+  bool isFishTasty = false;
+  cout << isCodingFun << "\n";
+  cout << isFishTasty;
+  return 0;
+}
+#1
+#0
