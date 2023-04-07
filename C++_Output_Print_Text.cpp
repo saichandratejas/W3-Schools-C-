4d4f5867ebaa6df85c98fa11ec7b19af18d181ca
sai_tejas <saitejassharma@gmail.com>
2023-04-07 21:57:34 +0530

    Create C++_Output_Print_Text.cpp

---
+++ C++_Output_Print_Text.cpp
@@ -0,0 +1,20 @@
+> Ex:1
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+> Ex:2  
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  cout << "I am learning C++";
+  return 0;
+}
