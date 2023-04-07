6f0502ca25cd6fe2412841c1b726252207586572
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:15:36 +0530

    Create C++ Declare_Multiple_Variables.cpp

---
+++ C++ Declare_Multiple_Variables.cpp
@@ -0,0 +1,26 @@
+> Ex: 1 - Declare Many Variables
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5, y = 6, z = 50;  
+  cout << x + y + z;
+  return 0;
+}                         # 61
+
+> Ex: 2 - One Value to Multiple Variables
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y, z;
+  x = y = z = 50;
+  cout << x + y + z;
+  return 0;
+}                          # 150
+  
+  
+
+
