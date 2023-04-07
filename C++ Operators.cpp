8d40477a839946d984f44cfe92ee6bec404298ec
sai_tejas <saitejassharma@gmail.com>
2023-04-07 23:06:30 +0530

    Create C++ Operators.cpp

---
+++ C++ Operators.cpp
@@ -0,0 +1,30 @@
+> Ex : 1 - Operators
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 100 + 50;
+  cout << x;
+  return 0;
+}                             # 150
+
+> Ex : 2 - Operators
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int sum1 = 100 + 50;        // 150 (100 + 50)
+  int sum2 = sum1 + 250;      // 400 (150 + 250)
+  int sum3 = sum2 + sum2;     // 800 (400 + 400)
+  cout << sum1 << "\n";
+  cout << sum2 << "\n";
+  cout << sum3;
+  return 0;
+}                            # 150
+                             # 400
+                             # 800
+  
+  
+
