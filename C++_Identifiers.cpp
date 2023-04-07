e068246fbad4684e676fae002ecfa0a37bf058e1
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:17:40 +0530

    Create C++_Identifiers.cpp

---
+++ C++_Identifiers.cpp
@@ -0,0 +1,19 @@
+> Ex: 1 - C++ Identifiers
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Good name
+  int minutesPerHour = 60;
+  
+  // OK, but not so easy to understand what m actually is
+  int m = 60;
+  
+  cout << minutesPerHour << "\n";
+  cout << m;
+  return 0;
+  }                        # 60
+                           # 60
+  
+  
