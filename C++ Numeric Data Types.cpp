3c6d41e814ca02d67cee8be4758e90cb153119d3
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:44:07 +0530

    Create C++ Numeric Data Types.cpp

---
+++ C++ Numeric Data Types.cpp
@@ -0,0 +1,33 @@
+> Ex : 1 - int
+  
+#include <iostream>
+using namespace std;
+ 
+int main () {
+  int myNum = 1000;
+  cout << myNum;
+  return 0;
+}
+
+> Ex : 2 - float
+  
+#include <iostream>
+using namespace std;
+ 
+int main () {
+  float myNum = 5.75;
+  cout << myNum;
+  return 0;
+}
+
+> Ex : 3 - double
+
+#include <iostream>
+using namespace std;
+ 
+int main () {
+  double myNum = 19.99;
+  cout << myNum;
+  return 0;
+}
+
