ecc73025d17b2e0f5e1702b14ea99704b04cd8bd
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:45:46 +0530

    Create C++ Boolean Data Types.cpp

---
+++ C++ Boolean Data Types.cpp
@@ -0,0 +1,16 @@
+> Ex : 1 - Boolean
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  bool CodingFun = true;
+  bool FishTasty = false;
+  cout << CodingFun << "\n";
+  cout << FishTasty;
+  return 0;
+}
+                   # 1
+                   # 0
+
+
