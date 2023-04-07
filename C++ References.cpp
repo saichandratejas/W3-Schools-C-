65c573bb7743f6d124643ef980fbf4ff1737d709
sai_tejas <saitejassharma@gmail.com>
2023-04-08 01:20:23 +0530

    Update C++ References.cpp

--- C++ References.cpp
+++ C++ References.cpp
@@ -3,9 +3,29 @@ Creating References:
   
 >> Ex:
 
-string food = "Pizza";  // food variable
+string food = " Pizza";  // food variable
 string &meal = food;    // reference to food 
 
 // output :
 Pizza
 Pizza
+
+
+Memory Address:
+- can also be used to get the memory address of a variable; which is the location of where the variable is stored on the computer.
+- When a variable is created in C++, a memory address is assigned to the variable. And when we assign a value to the variable, it is stored in this memory address.
+- To access it, use the & operator, and the result will represent where the variable is stored.
+  
+  > Ex :
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";
+
+  cout << &food;
+  return 0;
+}
+// output: 0x6dfed4
