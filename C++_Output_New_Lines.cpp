1e1edcfaf24952914806490c1fbbcd548d757744
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:01:02 +0530

    Create C++_Output_New_Lines.cpp

---
+++ C++_Output_New_Lines.cpp
@@ -0,0 +1,36 @@
+> Ex:1
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "I am learning C++";
+  return 0;
+}
+
+> Ex:2
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n\n";
+  cout << "I am learning C++";
+  return 0;
+}
+
+- Another way to insert a new line, is with the endl manipulator
+
+> Ex:3
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "I am learning C++";
+  return 0;
+}
+
+
