81574037e68f953e1bcb5fbd6a6df230e4d181e6
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:02:48 +0530

    Update C++_Comments.cpp

--- C++_Comments.cpp
+++ C++_Comments.cpp
@@ -1,2 +1,15 @@
+> Ex:1
+  
 // This is a comment
 cout << "Hello World!";
+
+> Ex:2
+  
+cout << "Hello World!"; // This is a comment
+
+> Ex:3
+  
+/* The code below will print the words Hello World!
+to the screen, and it is amazing */
+cout << "Hello World!";
+
