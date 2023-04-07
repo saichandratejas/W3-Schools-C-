58553c7085757dfbad59ab6fe0c1e75d67addc53
sai_tejas <saitejassharma@gmail.com>
2023-04-07 23:10:30 +0530

    Create C++ Comparison Operators.cpp

---
+++ C++ Comparison Operators.cpp
@@ -0,0 +1,24 @@
+> Ex : 1 - Comparison Operators
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << (x > y); // returns 1 (true) because 5 is greater than 3
+  return 0;
+}                                # 1
+  
+  
+> Ex : 2 - 
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << (x < y); // returns 0 (false) because 5 is greater than 3
+  return 0;
+}                                 # 0
