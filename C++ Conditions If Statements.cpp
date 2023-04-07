8a0d25fece893b74021b602c94579adee33e35cf
sai_tejas <saitejassharma@gmail.com>
2023-04-07 23:28:48 +0530

    Create C++ Conditions If Statements.cpp

---
+++ C++ Conditions If Statements.cpp
@@ -0,0 +1,26 @@
+> Ex : 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  if (20 > 18) {
+    cout << "20 is greater than 18";
+  }  
+  return 0;
+}                               # 20 is greater than 18
+
+
+> Ex : 2
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 20;
+  int y = 18;
+  if (x > y) {
+    cout << "x is greater than y";
+  }  
+  return 0;
+}                                # x is greater than y
