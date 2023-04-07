5deff7f3a540d8681d1b3fc9ce5bd300865adbc0
sai_tejas <saitejassharma@gmail.com>
2023-04-07 22:57:50 +0530

    Create C++ Character Data Types.cpp

---
+++ C++ Character Data Types.cpp
@@ -0,0 +1,24 @@
+> Ex : 1 - Character Types
+
+#include <iostream>
+using namespace std;
+ 
+int main () {
+  char myGrade = 'B';
+  cout << myGrade;
+  return 0;           # B
+}
+
+> Ex : 2 - Alternatively, you can use ASCII values to display certain characters
+  
+ #include <iostream>
+using namespace std;
+ 
+int main () {
+  char a = 97, b = 66, c = 99;
+  cout << "The value is :"<< a;
+  cout << b;
+  cout << c;
+  return 0;
+}                       # aBc
+
