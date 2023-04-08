9da853266e2a34d9344b1c9fb5374b3f43b38716
sai_tejas <saitejassharma@gmail.com>
2023-04-09 01:30:40 +0530

    Create C++ Pass Array to a Function.cpp

---
+++ C++ Pass Array to a Function.cpp
@@ -0,0 +1,19 @@
+void myFunction(int myNumbers[5]) {
+  for (int i = 0; i < 5; i++) {
+    cout << myNumbers[i] << "\n";
+  }
+}
+
+int main() {
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  myFunction(myNumbers);
+  return 0;
+}
+
+// output :
+
+10
+20
+30
+40
+50
