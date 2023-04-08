599e8565f1b566e88ed2086ebf74cb8edf48755a
sai_tejas <saitejassharma@gmail.com>
2023-04-08 21:48:55 +0530

    Create C++_Pointers.cpp

---
+++ C++_Pointers.cpp
@@ -0,0 +1,54 @@
+- A pointer however, is a variable that stores the memory address as its value.
+  
+> Ex : 1
+  
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";  // A string variable
+  string* ptr = &food;  // A pointer variable that stores the address of food
+
+  // Output the value of food
+  cout << food << "\n";
+
+  // Output the memory address of food
+  cout << &food << "\n";
+
+  // Output the memory address of food with the pointer
+  cout << ptr << "\n";
+  return 0;
+}
+
+
+
+> Ex : 2 - Modify Pointers
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";
+  string* ptr = &food;
+
+  // Output the value of food
+  cout << food << "\n";
+
+  // Output the memory address of food
+  cout << &food << "\n";
+
+  // Access the memory address of food and output its value
+  cout << *ptr << "\n";
+  
+  // Change the value of the pointer
+  *ptr = "Hamburger";
+  
+  // Output the new value of the pointer
+  cout << *ptr << "\n";
+  
+  // Output the new value of the food variable
+  cout << food << "\n";
+  return 0;
+}
