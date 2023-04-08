09d504bb3f3d8e58a693ab6dd9c907ba154d6842
sai_tejas <saitejassharma@gmail.com>
2023-04-09 01:25:23 +0530

    Create C++ The Return Keyword.cpp

---
+++ C++ The Return Keyword.cpp
@@ -0,0 +1,39 @@
+> Ex : 1 
+  
+int myFunction(int x) {
+  return 5 + x;
+}
+
+int main() {
+  cout << myFunction(3);
+  return 0;
+}
+
+// Outputs 8 (5 + 3)
+
+> Ex : 2 - returns the sum of a function with two parameters
+  
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  cout << myFunction(5, 3);
+  return 0;
+}
+
+// Outputs 8 (5 + 3)
+
+
+> Ex : 3 - You can also store the result in a variable
+  
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  int z = myFunction(5, 3);
+  cout << z;
+  return 0;
+}
+// Outputs 8 (5 + 3)
