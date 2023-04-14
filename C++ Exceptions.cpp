5143ca1b8eae5133ce011b10feb75ab623832656
sai_tejas <saitejassharma@gmail.com>
2023-04-14 09:57:07 +0530

    Update C++ Exceptions.cpp

--- C++ Exceptions.cpp
+++ C++ Exceptions.cpp
@@ -10,6 +10,8 @@ catch () {
   // Block of code to handle errors
 }
 
+> Eg : 1
+ 
 #include <iostream>
 using namespace std;
 
@@ -30,3 +32,26 @@ int main() {
 }
 // Access denied - You must be at least 18 years old.
 // Age is: 15
+
+> Eg : 2
+ 
+ #include <iostream>
+using namespace std;
+
+int main() {
+  try {
+    int age = 15;
+    if (age >= 18) {
+      cout << "Access granted - you are old enough.";
+    } else {
+      throw 505;
+    }
+  }
+  catch (int myNum) {
+    cout << "Access denied - You must be at least 18 years old.\n";
+    cout << "Age is:"<<myNum;
+  }
+  return 0;
+}
+// Access denied - You must be at least 18 years old.
+// Age is:505
