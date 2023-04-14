06e09cd6d2bfdb2922338b8a62bba4cfe9bc3524
sai_tejas <saitejassharma@gmail.com>
2023-04-14 09:35:25 +0530

    Create C++ Inheritance.cpp

---
+++ C++ Inheritance.cpp
@@ -0,0 +1,29 @@
+- To inherit from a class, use the : symbol.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Base class
+class Vehicle {
+  public: 
+    string brand = "Ford";
+    void honk() {
+      cout << "Tuut, tuut! \n" ;
+    }
+};
+
+// Derived class
+class Car: public Vehicle {
+  public: 
+    string model = "Mustang";
+};
+
+int main() {
+  Car myCar;
+  myCar.honk();
+  cout << myCar.brand + " " + myCar.model;
+  return 0;
+}
+// Tuut , tuut!
+// Ford Mustang
